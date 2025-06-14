321e79ba6bcf86d3a7870f077a9cc2bbd10c81a9
Jorge  Alcaíno Brevis <jorge.alcaino@alumnos.uv.cl>
2025-06-14 15:15:23 -0400

    Update urls.py

--- asignacion_api/gestor_vehiculos/urls.py
+++ asignacion_api/gestor_vehiculos/urls.py
@@ -5,10 +5,12 @@ from django.urls import path, include
 # 1. Importaciones necesarias para servir archivos multimedia
 from django.conf import settings
 from django.conf.urls.static import static
+from asignaciones.views import UserGroupView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('asignaciones.urls')),
+    path('api/user-groups/', UserGroupView.as_view(), name='user-groups'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # 2. Añade esta condición al final del archivo
